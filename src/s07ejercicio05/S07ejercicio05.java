66d9d1da93d5e5662e65a6fe01acef8798f8c921
WilyPisco <wilmerpisco@users.noreply.github.com>
2017-09-03 14:15:34 -0500

    first

---
+++ src/s07ejercicio05/S07ejercicio05.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s07ejercicio05;
+
+/**
+ *
+ * @author wily1
+ */
+public class S07ejercicio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       int diasAnio=365;
+       int horasDia=24;
+       int minutosHora=60;
+       int segundosMinuto=60;
+       
+       int segundosAnio=diasAnio*horasDia*minutosHora*segundosMinuto;
+       int segundosAniox2=segundosAnio*2;
+       
+       System.out.println("Hay "+ segundosAnio+ " segundos en un año");
+       System.out.println("Hay "+segundosAniox2+" segundos en dos años");
+    }
+    
+}
